5af8e88f96ca2d807c46ebd636a6b44ed0b717c4
kale0224 <65206906+kale0224@users.noreply.github.com>
2020-05-18 14:41:03 +0900

    Update 20200518_teamsorting.cpp

--- TeamSorting/20200518_teamsorting.cpp
+++ TeamSorting/20200518_teamsorting.cpp
@@ -14,6 +14,8 @@ struct Team
 	char company[MAXLEN];
 };
 
+void print_list(struct Team* tm, int tnum);
+
 int main()
 {
 	Team t[MAX];
@@ -46,13 +48,82 @@ int main()
 				getchar();
 				printf("팀명을 입력해주세요[19자까지 입력가능] : ");
 				fgets(t[tnum].name, MAXLEN, stdin);
-				printf("회사명을 입력해주세요[19자까지 입력가능] : ");
 
+				printf("회사명을 입력해주세요[19자까지 입력가능] : ");
 				fgets(t[tnum].company, MAXLEN, stdin);
-				t[tnum].id = tnum++;
+
+				t[tnum++].id = tnum + 1;
 				break;
 			case 2:
 				//박윤화
+				int a;
+
+				print_list(t, tnum);
+
+				printf("수정할 팀ID를 선택하세요 : \n");
+				scanf_s("%d", &a);
+				for (int i = 0; i < tnum; i++)
+				{
+					if (a == t[i].id)
+					{
+						while (1)
+						{
+							int b;
+
+							printf("수정할 항목을 선택하세요 : \n");
+							printf("1. 팀명\n");
+							printf("2. 회사명\n");
+							scanf_s("%d", &b);
+
+							if (b == 1)
+							{
+								char c[MAXLEN];
+
+								getchar();
+								printf("수정할 팀명 : \t");
+								fgets(c, MAXLEN, stdin);
+								strcpy(t[i].name, c);
+							}
+
+							else if (b == 2)
+							{
+								char d[MAXLEN];
+
+								getchar();
+								printf("수정할 회사명 : \t");
+								fgets(d, MAXLEN, stdin);
+								strcpy(t[i].company, d);
+							}
+
+							else
+							{
+								printf("다시 선택하세요 : \n");
+								continue;
+							}
+							break;
+						}
+
+					}
+				}
+				print_list(t, tnum);
+
+				break;
+			case 3:
+				int e;
+
+				print_list(t, tnum);
+
+				printf("삭제할 팀ID를 선택하세요 : \n");
+				scanf_s("%d", &e);
+
+				for (int i = 0; i < tnum; i++)
+				{
+					if (e >= t[i].id)
+					{
+						t[i] = t[i + 1];
+					}
+				}
+				tnum--;
 				break;
 			default:
 				printf("값을 다시 입력해주세요\n");
@@ -128,4 +199,15 @@ int main()
 		}
 	}
 	return 0;
+}
+
+void print_list(struct Team* tm, int tnum)
+{
+	for (int i = 0; i < tnum; i++)
+	{
+		printf("%d\t", (tm + i)->id);
+		for (int j = 0; j < strlen((tm + i)->name) - 1; j++)
+			printf("%c", (tm + i)->name[j]);
+		printf("\t%s\n", (tm + i)->company);
+	}
 }

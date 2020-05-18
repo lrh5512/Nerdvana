be3b0ba8edd47d39c72534cc1acd4699c076c27c
dodyd <chaste_lady@naver.com>
2020-05-18 16:59:12 +0900

    add fileIO by cw

--- TeamSorting/20200518_teamsorting.cpp
+++ TeamSorting/20200518_teamsorting.cpp
@@ -14,13 +14,16 @@ struct Team
 	char company[MAXLEN];
 };
 
-void print_list(struct Team* tm, int tnum);
+void print_list(struct Team* t, int tnum);
+int fileIO(Team* t, int tnum, char buf);
 
 int main()
 {
 	Team t[MAX];
 	int tnum = 0;
 	int sel1, sel2;
+
+	tnum = fileIO(t, tnum, 'r');	// r : read, w : write
 
 	while (1)
 	{
@@ -31,8 +34,8 @@ int main()
 		switch (sel1)
 		{
 		case 1:
-			system("cls");
-			printf("1. 팀 정보 추가\n");
+			//system("cls");
+			printf("\n1. 팀 정보 추가\n");
 			printf("2. 팀 정보 수정\n");
 			printf("3. 팀 정보 삭제\n");
 			scanf("%d", &sel2);
@@ -46,13 +49,21 @@ int main()
 					break;
 				}
 				getchar();
+
+				char tmp[MAXLEN];
+				printf("팀번호를 입력해주세요 : ");
+				fgets(tmp, MAXLEN, stdin);
+				t[tnum].id = atoi(tmp);
+
 				printf("팀명을 입력해주세요[19자까지 입력가능] : ");
 				fgets(t[tnum].name, MAXLEN, stdin);
+				t[tnum].name[strlen(t[tnum].name) - 1] = '\0';
 
 				printf("회사명을 입력해주세요[19자까지 입력가능] : ");
 				fgets(t[tnum].company, MAXLEN, stdin);
+				t[tnum].company[strlen(t[tnum].company) - 1] = '\0';
 
-				t[tnum++].id = tnum + 1;
+				tnum++;
 				break;
 			case 2:
 				//박윤화
@@ -93,6 +104,7 @@ int main()
 								printf("수정할 회사명 : \t");
 								fgets(d, MAXLEN, stdin);
 								strcpy(t[i].company, d);
+								t[i].company[strlen(t[i].company) - 1] = '\0';
 							}
 
 							else
@@ -148,10 +160,10 @@ int main()
 						{
 							if (t[j].id > t[j + 1].id)
 							{
-								int temp;
-								temp = t[j].id;
-								t[j].id = t[j + 1].id;
-								t[j + 1].id = temp;
+								Team temp;
+								temp = t[j];
+								t[j] = t[j + 1];
+								t[j + 1] = temp;
 							}
 						}
 					}
@@ -192,22 +204,99 @@ int main()
 				}
 				break;
 			}
+			break;
 		case 3:
-			printf("프로그램이 종료됩니다.\n");
-			exit(0);
+			getchar();
+
+			char res;
+			printf("저장하시겠습니까?(y/n)");
+			scanf("%c", &res);
+			while (1)
+			{
+				switch (res)
+				{
+				case 'y':
+					fileIO(t, tnum, 'w');
+					printf("저장되었습니다.\n");
+					exit(0);
+					break;
+				case 'n':
+					exit(0);
+					break;
+				default:
+					printf("y나 n을 입력해주세요.\n");
+				}
+			}
 			break;
 		}
 	}
 	return 0;
 }
 
-void print_list(struct Team* tm, int tnum)
+void print_list(struct Team* t, int tnum)
 {
 	for (int i = 0; i < tnum; i++)
 	{
-		printf("%d\t", (tm + i)->id);
-		for (int j = 0; j < strlen((tm + i)->name) - 1; j++)
-			printf("%c", (tm + i)->name[j]);
-		printf("\t%s\n", (tm + i)->company);
+		printf("%d\t", (t + i)->id);
+		for (int j = 0; j < strlen((t + i)->name) - 1; j++)
+			printf("%c", (t + i)->name[j]);
+		printf("\t%s\n", (t + i)->company);
 	}
+}
+
+int fileIO(Team* t, int tnum, char buf)
+{
+	FILE* f;
+	switch (buf)
+	{
+	case 'r':
+		if ((f = (fopen("data.txt", "r"))) != 0)
+		{
+			char line[MAXLEN * 3];
+			char* token;
+
+			while (feof(f) == 0)
+			{
+				fgets(line, sizeof(line), f);
+				if (line[0] == '\0') break;
+				token = strtok(line, "\t");
+				t[tnum].id = atoi(token);
+				token = strtok(NULL, "\t");
+				strcpy(t[tnum].name, token);
+				token = strtok(NULL, "\n");
+				strcpy(t[tnum].company, token);
+				memset(line, '\0', sizeof(line));
+				tnum++;
+			}
+		}
+		else
+			printf("오류!\n");
+		break;
+	case 'w':
+		if ((f = (fopen("data.txt", "w"))) != 0)
+		{
+			for (int i = 0; i < tnum; i++)
+			{
+				char line[MAXLEN * 3];
+
+				_itoa(t[i].id, line, 10);
+				strcat(line, "\t");
+
+				strcat(line, t[i].name);
+				strcat(line, "\t");
+
+				strcat(line, t[i].company);
+				strcat(line, "\n");
+
+				fputs(line, f);
+			}
+		}
+		else
+			printf("오류!\n");
+		break;
+	default:
+		exit(1);
+	}
+	fclose(f);
+	return tnum;
 }
